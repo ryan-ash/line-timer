ef64324f5f8b5d85b9cb7be368dd8049b33860c5
Ryan Ash <scarletspark@gmail.com>
2018-09-13 02:21:22 +0300

    timezone detection added

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     var diff = config["timer"]["minutes-to-countdown"];
     var countdown_date_obj = new Date(Date.now() + diff*60000);
-    var timezone = " GMT+0300";
+    var timezone = " " + new Date().toString().match(/([A-Z]+[\+-][0-9]+)/)[1];
     var date_till = config["timer"]["fixed-date"]
         ? config["timer"]["date"] + timezone
         : dateFormat(countdown_date_obj, "mmmm d yyyy HH:MM:ss") + timezone;
-
-    console.log(date_till);
 
     var $digits;
     var $colon;
